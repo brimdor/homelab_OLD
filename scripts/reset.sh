16af904d94740c61353123c7d9f6db2eac2deae8
brimdor <chrisnelsonx@gmail.com>
2024-02-10 00:16:27 -0600

    Update reset.sh

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+echo "Stopping Docker Containers"
 docker stop files_http_1 files_dnsmasq_1
+
+echo "Removing Docker Containers"
 docker rm files_http_1 files_dnsmasq_1
+
+echo "Pruning Images"
 docker image prune -a --force
 
+echo "Removing Cache Containers"
+docker volume rm homelab-tools-cache homelab-tools-nix
+
+echo "Removing terraform.d folder"
 sudo rm -Rf ~/.terraform.d
 
+echo "Removing homelab folder"
 cd ~
 sudo rm -Rf homelab
+
+echo "And Bob is your uncle!"
